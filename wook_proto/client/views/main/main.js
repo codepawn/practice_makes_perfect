3a258a542a850f8a6f7847964cebb34113ad0825
Lafull <codepawn@icloud.com>
2015-01-14 09:48:30 +0900

    Pure css test

--- wook_proto/client/views/main/main.js
+++ wook_proto/client/views/main/main.js
@@ -1,28 +1,28 @@
 Template.Main.rendered = function () {
-
-  (function ($) {
-    var handler = $('li');
-    $('#main').imagesLoaded(function () {
-      handler.wookmark({
-        // Prepare layout options.
-        autoResize: true, // This will auto-update the layout when the browser window is resized.
-        container: $('#main'), // Optional, used for some extra CSS styling
-        offset: 0, // Optional, the distance between grid items
-        outerOffset: 0, // Optional, the distance to the containers border
-        itemWidth: 210 // Optional, the width of a grid item
-      });
-    });
-    // Capture clicks on grid items.
-    handler.click(function () {
-      // Randomize the height of the clicked item.
-      var newHeight = $('img', this).height() + Math.round(Math.random() * 300 + 30);
-      $(this).css('height', newHeight + 'px');
-
-      // Update the layout.
-      handler.wookmark();
-    });
-
-  })(jQuery);
+  //
+  //(function ($) {
+  //  var handler = $('li');
+  //  $('#main').imagesLoaded(function () {
+  //    handler.wookmark({
+  //      // Prepare layout options.
+  //      autoResize: true, // This will auto-update the layout when the browser window is resized.
+  //      container: $('#main'), // Optional, used for some extra CSS styling
+  //      offset: 0, // Optional, the distance between grid items
+  //      outerOffset: 0, // Optional, the distance to the containers border
+  //      itemWidth: 210 // Optional, the width of a grid item
+  //    });
+  //  });
+  //  // Capture clicks on grid items.
+  //  handler.click(function () {
+  //    // Randomize the height of the clicked item.
+  //    var newHeight = $('img', this).height() + Math.round(Math.random() * 300 + 30);
+  //    $(this).css('height', newHeight + 'px');
+  //
+  //    // Update the layout.
+  //    handler.wookmark();
+  //  });
+  //
+  //})(jQuery);
 
 
   //(function ($){
