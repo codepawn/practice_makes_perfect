066f4bdf337c7d808a072960b284de974bccbbfc
Hyun Ahn <codepawn@icloud.com>
2015-09-01 09:57:42 +0900

    Python, Swift

--- playground/python/notepad2.py
+++ playground/python/notepad2.py
@@ -1,25 +1,23 @@
 prices = {
-    'banana': 4,
-    'apple': 2,
-    'orange': 1.5,
-    'pear': 3
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3,
 }
-
-
 stock = {
-    'banana': 6,
-    'apple': 0,
-    'orange': 32,
-    'pear': 15
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15,
 }
 
 for key in prices:
-    total = 0
     print key
     print "price: %s" % prices[key]
-    print 'stock: %s' % stock[key]
+    print "stock: %s" % stock[key]
 
-    for in stock[key]
+total = prices[key] * stock[key]
+print total
 
 -0.6 + 0.25 + 0.125
 -1.28 + 0.625
@@ -32,3 +30,48 @@ import math
 print math.pow(270, 1 / 3.0)
 print math.sqrt(128)
 print math.sqrt(512)
+
+# Write your code below!
+listItems = [1, 2, 3, 4, 5]
+
+
+def goo():
+    total = 0
+    for num in listItems:
+        total += num
+    print total
+goo()
+
+
+groceries = [
+    'banana',
+    'orange',
+    'apple'
+]
+
+
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+def compute_bill(food):
+    total = 0
+    for item in food:
+        if stock[item] > 0:
+            total += prices[item]
+            stock[item] -= 1
+    return total
+
+print compute_bill(shopping_list)
--- playground/swift/MyPlayground.playground/Contents.swift
+++ playground/swift/MyPlayground.playground/Contents.swift
@@ -13,3 +13,25 @@ var three :Int = 3
 var colorsArray = ["Blue", "Red", "Green", "Yellow"]
 
 var colorsDictionary = ["PrimaryColor":"Green", "SecondaryColor":"Red"]
+
+let power_number: Int = 20
+let next_string: String = "googooogo"
+var google = "google"
+google
+google = "gooo"
+google
+
+
+
+var greeting: String = "hello"
+greeting = "goo"
+
+let languageName: String = "Swift"
+var version: Double = 1.0
+let introduced: Double = 2014
+
+let isAwesome: Bool = true
+isAwesome
+introduced
+var version = 1.0
+version
